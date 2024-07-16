8480bef196fcfd0e254b76e8b5ab7473fd85cf07
may <soyeon1056@naver.com>
2024-07-16 19:07:25 +0900

    7월2주차

---
+++ week7_2/14.4.js
@@ -0,0 +1,15 @@
+//덧칠하기
+function solution(n, m, section) {
+  let answer = 0;
+
+  let cur = 0;
+
+  for (let i = 0; i < section.length; i++) {
+    if (cur < section[i]) {
+      cur = section[i] + m - 1;
+      answer++;
+    }
+  }
+
+  return answer;
+}
